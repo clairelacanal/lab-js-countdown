263ab9127e09d1add8c3a7d056d36133e56f135f
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-18 16:16:12 +0200

    lab on going

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+let buttonStart = document.querySelector("#start-btn");
+buttonStart.addEventListener('click', function(){
+  startCountdown();
+})
 
 
 
@@ -14,25 +18,33 @@ let timer = null; // Variable to store the interval
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
+  document.getElementById("start-btn").disabled = true;
+  timer = document.querySelector("#time");
 
-
-  // Your code goes here ...
+  const intervalId = setInterval(function () {
+    if(remainingTime < 0) {
+      clearInterval(intervalId);
+      showToast();
+      return;
+    }
+    timer.innerHTML = remainingTime;
+    remainingTime--;
+  }, 1000);
+  
 }
-
-
 
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
 
-  // Your code goes here ...
+  let card = document.querySelector("#toast");
+  card.classList.add("shown");
 
-
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  
 }
+
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+
+  // Your code goes here ...
+
